eaa607102330eb9c909ef2f3e1aeb95483d6ab22
vandim199 <vandim@abv.bg>
2021-02-09 11:29:13 +0200

    Jumping Added

--- GXPEngine/GXPEngine/GameLoader.cs
+++ GXPEngine/GXPEngine/GameLoader.cs
@@ -9,17 +9,19 @@ namespace GXPEngine
     class GameLoader : GameObject
     {
         Player1 player1;
+        public static Canvas floor;
 
         public GameLoader() : base(false)
         {
             new Backdrop("Gameplay.png");
+
+            floor = new Canvas(1920, 200);
+            floor.SetXY(0, 830);
+            floor.graphics.FillRectangle(new SolidBrush(Color.Black), new Rectangle(0, 0, 1920, 300));
+            AddChild(floor);
+
             player1 = new Player1();
             AddChild(player1);
-
-            Canvas canvas = new Canvas(1920, 200);
-            canvas.SetXY(0, 830);
-            canvas.graphics.FillRectangle(new SolidBrush(Color.Black), new Rectangle(0, 0, 1920, 300));
-            AddChild(canvas);
         }
 
         void Update()
--- GXPEngine/GXPEngine/Player1.cs
+++ GXPEngine/GXPEngine/Player1.cs
@@ -8,40 +8,70 @@ namespace GXPEngine
 {
     class Player1 : AnimationSprite
     {
-        float _speed = 7;
-        float _speedX, _speedY;
+        float _speed = 50;
+        float _speedX, _speedY = 20;
         float _timeJumped;
+        bool canJump;
+        bool _playingAnimation;
         public static int onFrame;
 
-        public Player1() : base("Fillia Kick.png", 8, 1, -1, false, false)
+        public Player1() : base("FilliaTest.png", 12, 2, -1, false, true)
         {
             scale = 0.8f;
-            SetXY(0, 200);
-            SetCycle(0, 8, 5);
+            SetXY(0, 0);
         }
 
         void Update()
         {
+            Animate();
             onFrame = currentFrame;
-            //if (_speedX != 0) Animate();
+
+            if (Input.GetKeyDown(Key.J))
+            {
+                SetCycle(12, 8, 5);
+                _playingAnimation = true;
+            }
+            if (currentFrame == 19) _playingAnimation = false;
+
+            if (!_playingAnimation)
+            {
+                if (_speedX != 0) SetCycle(0, 12, 5);
+                else SetCycle(0, 1, 5);
+            }
 
             if (Input.GetKey(Key.D)) _speedX = _speed;
             else if (Input.GetKey(Key.A)) _speedX = -_speed;
             else _speedX = 0;
 
-            if (Input.GetKeyDown(Key.W))
+            if (Input.GetKey(Key.W) && canJump)
             {
                 _timeJumped = Time.now;
-                //_speedY = 20;
+                _speedY = -20;
             }
 
-            //if (Time.now >= _timeJumped + 200) _speedY = -20;
+            if(Time.now >= _timeJumped + 200)
+            {
+                _speedY = 20;
+            }
+
+            if (!canJump)
+            {
+                _speed = 20;
+            }
+
+            if (this.collider.GetCollisionInfo(GameLoader.floor.collider) != null)
+            {
+                y -= 5;
+                canJump = true;
+                _speed = 50;
+            }
+            else canJump = false;
 
             MoveUntilCollision(_speedX, _speedY);
 
             if (Input.GetKey(Key.RIGHT)) Animate();
 
-            if (currentFrame == 2)
+            if (currentFrame == 14)
             {
                 Hurtbox hurtbox = new Hurtbox(100, 0, 500, 700, currentFrame);
                 AddChild(hurtbox);
@@ -49,7 +79,7 @@ namespace GXPEngine
                 Hitbox hitbox = new Hitbox(600, 10, 400, 300, currentFrame);
                 AddChild(hitbox);
             }
-            else if (currentFrame == 3)
+            else if (currentFrame == 15)
             {
                 Hurtbox hurtbox = new Hurtbox(100, 0, 500, 700, currentFrame);
                 AddChild(hurtbox);
